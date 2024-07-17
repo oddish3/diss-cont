7a8ac26c9239802edf057c52b93df37f90088ff1
oddish3 <solyates@gmail.com>
2024-07-17 19:48:23 +0100

    initial commit

---
+++ cont-twfe.R
@@ -0,0 +1,121 @@
+load("/home/oddish3/Downloads/medicare1.RData")
+head(medicare1)
+library(ggplot2)
+library(binscatteR)
+
+dose <- medicare1$medicare_share_1983
+dy <- medicare1$d_capital_labor_ratio
+summary(dy)
+summary(dose)
+
+ggplot(data.frame(dose=dose), aes(x=dose)) + 
+  geom_histogram()
+# a non trivial fraction of units are untreated while common values of traetment
+# are around 0.5 and this decreases as we move towards 0 and 1
+
+binnedout <- binscatter(data=medicare1, x="medicare_share_1983", y="d_capital_labor_ratio")
+binnedout
+
+twfe <- lm(dy ~ dose)
+summary(twfe)$coefficients
+# if we were hoping to estimate ACRT^o, then we did not do it well here, 
+# recall it is 0, but here the coefficient on the dose is 1.08 and strongly stat
+# diff from 0
+
+#' nonparametric estimates of att(d|d) and acrt(d|d)
+#' @param dy the change in the outcome over time
+#' @param dose the amount of the treatment
+#' @return list( 
+#'            local_effects - data frame containing the dose and estimates of 
+#'              att(dose) and acrt(dose)
+#'            att.overall - an estimate of the overall att
+#'            acrt.overall - an estimate of the overall acrt
+#'          )
+cont_did <- function(dy, dose) {
+  # choose bandwidth
+  bw <- np::npregbw(formula=dy ~ dose,
+                    regtype="ll",
+                    bws=1.06,
+                    bwscaling=TRUE,
+                    bandwidth.compute=FALSE)
+  # estimate att and acrt nonparametrically
+  out <- np::npreg(bws=bw, gradients=TRUE, exdat=dose)
+  
+  # order from smallest to largest dose and drop untreated
+  this_order <- order(dose)
+  dose <- dose[this_order]
+  dy <- dy[this_order]
+  att.d <- out$mean[this_order]
+  acrt.d <- out$grad[,1][this_order]
+  att.d <- att.d[dose>0]
+  acrt.d <- acrt.d[dose>0]
+  att.overall <- mean(att.d)
+  acrt.overall <- mean(acrt.d)
+  
+  return(list(local_effects=data.frame(dose=dose[dose>0],
+                                       att.d=att.d,
+                                       acrt.d=acrt.d),
+              att.overall=att.overall,
+              acrt.overall=acrt.overall))
+}
+#' @param l a particular value of the treatment for which to compute weights
+#' @param D an nx1 vector containing doses for all units
+cont_twfe_weights <- function(l, D) {
+  wt <- ( ( mean(D[D>=l]) - mean(D) ) * mean(1*(D>=l)) ) / var(D)
+  wt
+}
+
+
+
+plot_df <- cont_res$local_effects
+
+colnames(plot_df) <- c("dose", "att", "acrt")
+ggplot(plot_df, aes(x=dose, att)) +
+  geom_hline(yintercept=0, color="red", linetype="dashed") +
+  geom_line() +
+  theme_bw()
+
+ggplot(plot_df, aes(x=dose, acrt)) +
+  geom_hline(yintercept=0, color="red", linetype="dashed") +
+  geom_line() +
+  theme_bw()
+
+#-----------------------------------------------------------------------------
+dL <- min(dose[dose>0])
+dU <- max(dose)
+# density of the dose
+dose_grid <- seq(dL, dU, length.out=100)
+frq_weights_plot <- ggplot(data.frame(dose=dose[dose>0]), aes(x=dose)) +
+  geom_density(colour = "darkblue", linewidth = 1.2) +
+  xlim(c(min(dose_grid), max(dose_grid)))+
+  ylab("Density weights") +
+  xlab("Dose") +
+  ylim(c(0,3)) + 
+  labs(title="Density of dose")
+frq_weights_plot
+
+twfe_weights <- sapply(dose_grid, cont_twfe_weights, D=data$dose)
+cont_res <- cont_did(data$dy, data$dose)
+
+cont_res$att.overall
+cont_res$acrt.overall
+
+
+plot_df <- cbind.data.frame(twfe_weights, dose_grid)
+
+twfe_weights_plot <- ggplot(data=plot_df,
+                            mapping=aes(x = dose_grid,
+                                        y = twfe_weights)) +
+  geom_line(colour = "darkblue", linewidth = 1.2) +
+  xlim(c(min(dose_grid),
+         max(dose_grid)))+
+  ylab("TWFE weights") +
+  xlab("Dose") +
+  geom_vline(xintercept = mean(data$dose),
+             colour="black",
+             linewidth = 0.5,
+             linetype = "dotted") +
+  ylim(c(0,3)) +
+  labs(title="TWFE weights")
+
+twfe_weights_plot
