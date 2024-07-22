007993c1671ae5003f1cbf50b41c078ca0997936
oddish3 <solyates@gmail.com>
2024-07-22 17:28:20 +0100

    not working update

--- chen/run_nonlinear_regression.R
+++ chen/run_nonlinear_regression.R
@@ -4,150 +4,190 @@ cat("\014")
 
 library(splines2)
 library(tictoc)
+library(parallel)
+library(data.table)
 
-source("~/Documents/uni/master-dissertation/code-cont/chen/jhat.R")
-source("~/Documents/uni/master-dissertation/code-cont/chen/jlep.R")
-source("~/Documents/uni/master-dissertation/code-cont/chen/npiv.R")
-source("~/Documents/uni/master-dissertation/code-cont/chen/npiv_estimate.R")
-source("~/Documents/uni/master-dissertation/code-cont/chen/bspline.R")
-source("~/Documents/uni/master-dissertation/code-cont/chen/npiv_estimate.R")
-source("~/Documents/uni/master-dissertation/code-cont/chen/ucb_cv.R")
-source("~/Documents/uni/master-dissertation/code-cont/chen/ucb_cvge.R")
-source("~/Documents/uni/master-dissertation/code-cont/ucb_cc.R")
+# Define source files
+source_files <- c(
+  "~/Documents/uni/master-dissertation/code-cont/chen/jhat.R",
+  "~/Documents/uni/master-dissertation/code-cont/chen/jlep.R",
+  "~/Documents/uni/master-dissertation/code-cont/chen/npiv.R",
+  "~/Documents/uni/master-dissertation/code-cont/chen/npiv_estimate.R",
+  "~/Documents/uni/master-dissertation/code-cont/chen/bspline.R",
+  "~/Documents/uni/master-dissertation/code-cont/chen/ucb_cv.R",
+  "~/Documents/uni/master-dissertation/code-cont/chen/ucb_cvge.R",
+  "~/Documents/uni/master-dissertation/code-cont/ucb_cc.R"
+)
 
+# Source all required functions
+invisible(lapply(source_files, source))
+
+# Setup parameters
 array_value <- 1 # as.integer(Sys.getenv("SLURM_ARRAY_TASK_ID"))
-if (array_value <= 4) {
-  trimming <- TRUE
-  n_index <- array_value
-} else {
-  trimming <- FALSE
-  n_index <- array_value - 4
-}
-print(trimming)
-print(n_index)
+trimming <- if (array_value <= 4) TRUE else FALSE
+n_index <- if (trimming) array_value else array_value - 4
 
 # Inputs
-nn <- c(1250, 2500, 5000, 10000) # sample sizes
-nm <- 1000                      # number of replications
-nb <- 1000                      # number of bootstrap draws per replication
-nx <- 1000                      # number of points for grid of x values
-nL <- 9                         # maximum resolution level for J
-r <- 4                          # B-spline order
-M <- 5                          # ex ante upper bound on \sup_x h_0(x)
-alpha <- c(0.10, 0.05, 0.01)    # level of significance
+nn <- c(1250, 2500, 5000, 10000)
+nm <- 1000
+nb <- 1000
+nx <- 1000
+nL <- 9
+r <- 4
+M <- 5
+alpha <- c(0.10, 0.05, 0.01)
 nj <- 4
 
 # Pre-compute
 TJ <- 2^((0:nL) + 0) + r - 1
 CJ <- c(0, cumsum(TJ))
-Lhat <- rep(0, nm)
-Llep <- rep(0, nm)
-Ltil <- rep(0, nm)
-flag <- rep(0, nm)
-thet <- rep(0, nm)
-zast <- matrix(0, nm, length(alpha))
-zdet <- array(0, dim = c(nm, length(alpha), nj))
-cvge <- array(0, dim = c(nm, length(alpha), nj + 1))
-loss <- array(0, dim = c(nm, nj + 1))
-rati <- array(0, dim = c(nm, nj))
-bwd <- array(0, dim = c(980, 4, 10))
-
-Xx <- seq(0, 1, by = 1/nx)
-
-if (trimming == TRUE) {
-  Xx_sub <- Xx[Xx > 0.01 & Xx <= 0.99]
-} else {
-  Xx_sub <- Xx
-}
-# browser()
-Px <- matrix(0, nrow = length(Xx_sub), ncol = CJ[length(CJ)])
-for (ll in 0:nL) {
-  Px[, (CJ[ll + 1] + 1):CJ[ll + 2]] <- bspline(Xx_sub, ll, r)  # Note: bspline function needs to be defined in R
-}
-
+Xx <- seq(0, 1, length.out = nx + 1)
+Xx_sub <- if (trimming) Xx[Xx > 0.01 & Xx <= 0.99] else Xx
 h0 <- sin(15 * pi * Xx) * cos(Xx)
 
-# Simulations
-set.seed(1234567)
+# Precompute Px
+Px <- do.call(cbind, lapply(0:nL, function(ll) bspline(Xx_sub, ll, r)))
 
-n <- nn[n_index]
-
-for (j in 1:nm) { # j=1
+# Main simulation function
+run_simulation <- function(j, nn, n_index, nb, nx, nL, r, M, alpha, nj, TJ, CJ, Xx, Xx_sub, h0, Px) {
+  set.seed(1234567 + j)  # Ensure reproducibility in parallel
   
-  if (j %% 25 == 0) {
-    cat(sprintf("j = %d \n", j))
-  }
-  if (j == nm) {
-    cat("\n")
-  }
-  # browser()
+  log_file <- paste0("debug/debug_log_", j, ".txt")
+  
+  cat(paste("Running simulation for job:", j, "\n"), file = log_file)
+  
+  n <- nn[n_index]
+  
   # Simulate data
   x <- runif(n)
   u <- rnorm(n)
   y <- sin(15 * pi * x) * cos(x) + u
   
-  # Pre-compute basis functions and store in arrays PP and BB
-  PP <- matrix(0, nrow = n, ncol = CJ[length(CJ)])
-  for (ll in 0:nL) {
-    PP[, (CJ[ll + 1] + 1):CJ[ll + 2]] <- bspline(x, ll, r)
+  cat("Data simulation complete\n", file = log_file, append = TRUE)
+  
+  # Pre-compute basis functions
+  PP <- tryCatch({
+    do.call(cbind, lapply(0:nL, function(ll) bspline(x, ll, r)))
+  }, error = function(e) {
+    cat(paste("Error in creating PP:", e, "\n"), file = log_file, append = TRUE)
+    NULL
+  })
+  
+  if (is.null(PP)) {
+    cat("PP object not created\n", file = log_file, append = TRUE)
+    stop("PP object not found")
+  } else {
+    cat(paste("PP object created with dimensions:", dim(PP), "\n"), file = log_file, append = TRUE)
   }
   
-  # Compute \hat{J}_{\max} resolution level
-  tic()
-  result1 <- Jhat(PP, PP, CJ, CJ, TJ, M, n, nL) 
-  toc()
-  Lhat[j] <- result1$LL
-  flag[j] <- result1$flag
+  cat("Basis functions (PP) computed for job:", j, "\n", file = log_file, append = TRUE)
   
-  # Compute Lepski method resolution level
-  tic()
-  result2 <- Jlep(Lhat[j], Px, PP, PP, CJ, CJ, TJ, y, n, nb)
-  toc()
-  Llep[j] <- result2$LL
-  thet[j] <- result2$theta
+  # Compute resolution levels and estimates
+  result1 <- Jhat(PP, PP, CJ, CJ, TJ, M, n, nL)
+  result2 <- Jlep(result1$LL, Px, PP, PP, CJ, CJ, TJ, y, n, nb)
+  Ltil <- max(min(result2$LL, result1$LL - 1), 0)
   
-  # Compute \tilde{J} resolution level
-  Ltil[j] <- max(min(Llep[j], Lhat[j] - 1), 0)
+  cat("Resolution levels computed for job:", j, "\n", file = log_file, append = TRUE)
   
-  # Compute estimator and pre-asymptotic standard error
-  # debugonce(npiv_estimate)
-  npiv_result <- npiv_estimate(Ltil[j], Px, PP, PP, CJ, CJ, y, n)
-  hhat <- npiv_result$hhat
-  sigh <- npiv_result$sigh
+  npiv_result <- npiv_estimate(Ltil, Px, PP, PP, CJ, CJ, y, n)
   
-  # Compute critical value for UCB
-  zast[j, ] <- ucb_cv(Ltil[j], Lhat[j], Px, PP, PP, CJ, CJ, y, n, nb, 0, alpha)
+  cat("NPIV estimates computed for job:", j, "\n", file = log_file, append = TRUE)
   
-  # Compute sup-norm loss and excess width
-  loss[j, 1] <- max(abs(h0[which(Xx %in% Xx_sub)] - hhat))
+  # Compute critical values and coverage
+  zast_j <- ucb_cv(Ltil, result1$LL, Px, PP, PP, CJ, CJ, y, n, nb, 0, alpha)
+  cvge_result <- ucb_cvge(h0[which(Xx %in% Xx_sub)], npiv_result$hhat, npiv_result$sigh, zast_j, result2$theta, log(log(TJ[result2$LL + 1])))
   
-  # Compute coverage
-  result3 <- ucb_cvge(h0[which(Xx %in% Xx_sub)], hhat, sigh, zast[j, ], thet[j], log(log(TJ[Llep[j] + 1])))
-  cvge[j, , 1] <- result3$check
+  cat("Critical values and coverage computed for job:", j, "\n", file = log_file, append = TRUE)
   
-  for (k in 1:nj) {
-    # Compute undersmoothed estimator and pre-asymptotic standard error
-    result <- npiv_estimate(k + 2, Px, PP, PP, CJ, CJ, y, n)
-    hha1 <- result$hha
-    sig1 <- result$sig
-    
-    # Compute deterministic J critical value for undersmoothed UCB
-    zdet[j, , k] <- ucb_cc(k + 2, Px, PP, PP, CJ, CJ, y, n, nb, 0, alpha)
-    
-    # Compute sup-norm loss and excess width
-    loss[j, 1 + k] <- max(abs(h0[Xx %in% Xx_sub] - hha1))
-    rati[j, k] <- max(sig1) / max(sigh)
-    
-    # Compute coverage
-    result4 <- ucb_cvge(h0[Xx %in% Xx_sub], hha1, sig1, zdet[j, , k], 0, 0)
-    cvge[j, , 1 + k] <- result4$check
-  }
+  # Compute undersmoothed estimates
+  us_results <- lapply(1:nj, function(k) {
+    res <- npiv_estimate(k + 2, Px, PP, PP, CJ, CJ, y, n)
+    zdet_k <- ucb_cc(k + 2, Px, PP, PP, CJ, CJ, y, n, nb, 0, alpha)
+    cvge_k <- ucb_cvge(h0[Xx %in% Xx_sub], res$hha, res$sig, zdet_k, 0, 0)
+    list(hha1 = res$hha, sig1 = res$sig, zdet_k = zdet_k, cvge_k = cvge_k$check)
+  })
+  
+  cat("Undersmoothed estimates computed for job:", j, "\n", file = log_file, append = TRUE)
+  
+  # Compile results
+  result <- list(
+    Lhat = result1$LL, Llep = result2$LL, Ltil = Ltil,
+    flag = result1$flag, thet = result2$theta,
+    zast = zast_j, hhat = npiv_result$hhat, sigh = npiv_result$sigh,
+    cvge = c(cvge_result$check, sapply(us_results, function(x) x$cvge_k)),
+    loss = c(max(abs(h0[which(Xx %in% Xx_sub)] - npiv_result$hhat)),
+             sapply(us_results, function(x) max(abs(h0[Xx %in% Xx_sub] - x$hha1)))),
+    rati = sapply(us_results, function(x) max(x$sig1) / max(npiv_result$sigh)),
+    zdet = sapply(us_results, function(x) x$zdet_k)
+  )
+  
+  # Save intermediate results for debugging
+  save(result, file = paste0("result_debug_", j, ".RData"))
+  
+  cat("Results saved for job:", j, "\n", file = log_file, append = TRUE)
+  
+  return(result)
 }
 
-colMeans(loss[1:12,])
-colMeans(cvge[1:12, , ])
-colMeans(rati[1:12,])
+# debugonce(run_simulation)
+# run_simulation(1, nn, n_index, nb, nx, nL, r, M, alpha, nj, TJ, CJ, Xx, Xx_sub, h0, Px)
 
+# Run simulations in parallel
+cl <- makeCluster(detectCores() - 1)
+
+# Export all necessary objects and functions to the cluster
+clusterExport(cl, c("nn", "n_index", "nb", "nx", "nL", "r", "M", "alpha", "nj", "TJ", "CJ", "Xx", "Xx_sub", "h0", "Px", 
+                    "bspline", "Jhat", "Jlep", "npiv_estimate", "ucb_cv", "ucb_cvge", "ucb_cc"))
+
+
+# Load necessary libraries and source files on each cluster node
+clusterEvalQ(cl, {
+  library(splines2)
+  # source the necessary files directly
+  source("~/Documents/uni/master-dissertation/code-cont/chen/jhat.R")
+  source("~/Documents/uni/master-dissertation/code-cont/chen/jlep.R")
+  source("~/Documents/uni/master-dissertation/code-cont/chen/npiv.R")
+  source("~/Documents/uni/master-dissertation/code-cont/chen/npiv_estimate.R")
+  source("~/Documents/uni/master-dissertation/code-cont/chen/bspline.R")
+  source("~/Documents/uni/master-dissertation/code-cont/chen/ucb_cv.R")
+  source("~/Documents/uni/master-dissertation/code-cont/chen/ucb_cvge.R")
+  source("~/Documents/uni/master-dissertation/code-cont/ucb_cc.R")
+})
+
+
+tic()
+test_js <- c(1, 3, 4, 5, 7, 8, 9)
+sim_results <- parLapply(cl, test_js, run_simulation, 
+                         nn, n_index, nb, nx, nL, r, M, alpha, nj, TJ, CJ, Xx, Xx_sub, h0, Px)
+toc()
+
+stopCluster(cl)
+
+# Process results
+process_results <- function(sim_results) {
+  results <- data.table(
+    Lhat = sapply(sim_results, function(x) x$Lhat),
+    Llep = sapply(sim_results, function(x) x$Llep),
+    Ltil = sapply(sim_results, function(x) x$Ltil),
+    flag = sapply(sim_results, function(x) x$flag),
+    thet = sapply(sim_results, function(x) x$thet)
+  )
+  zast <- do.call(rbind, lapply(sim_results, function(x) x$zast))
+  zdet <- array(unlist(lapply(sim_results, function(x) x$zdet)), dim = c(nm, length(alpha), nj))
+  cvge <- do.call(rbind, lapply(sim_results, function(x) x$cvge))
+  loss <- do.call(rbind, lapply(sim_results, function(x) x$loss))
+  rati <- do.call(rbind, lapply(sim_results, function(x) x$rati))
+  
+  list(results = results, zast = zast, zdet = zdet, cvge = cvge, loss = loss, rati = rati)
+}
+
+final_results <- process_results(sim_results)
+
+# Output results
+print(colMeans(final_results$loss[1:12,]))
+print(colMeans(final_results$cvge[1:12,]))
+print(colMeans(final_results$rati[1:12,]))
+
+# Save results
 save_filename <- paste0("./results/regression_", array_value, ".RData")
-save(loss, cvge, rati, zdet, zast, Llep, Ltil, Lhat, thet, TJ, 
-     file = save_filename)
+save(list = c("final_results", "TJ"), file = save_filename)
